235b24354d5f321a528264ab074bb94b9bee70b4
Sushant Savilake <sushantsavilakedoc@gmail.com>
2026-02-05 21:05:58 +0530

    0

--- aws-resource-reporter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Enable debug mode
-set -x 
-
-# List S3, EC2, Lambda, and IAM
-aws s3 ls
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
-aws lambda list-functions
-aws iam list-users
